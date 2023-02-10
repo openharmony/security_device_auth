1afa7a6ffb009a255174c11261b8237662b63692
quyawei <quyawei1@huawei.com>
2023-02-10 16:13:42 +0800

    add TDD cases
    Signed-off-by: quyawei <quyawei1@huawei.com>

---
+++ test/unittest/deviceauth/unit_test/source/across_account_group_test.cpp
@@ -0,0 +1,180 @@
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cinttypes>
+#include <unistd.h>
+#include <gtest/gtest.h>
+
+#include "common_defs.h"
+#include "device_auth.h"
+#include "device_auth_defines.h"
+#include "hc_log.h"
+#include "json_utils.h"
+#include "../../../../../services/group_manager/src/group_operation/across_account_group/across_account_group.c"
+
+using namespace std;
+using namespace testing::ext;
+
+namespace {
+
+class AcrossAccountGroupTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+};
+
+void AcrossAccountGroupTest::SetUpTestCase() {}
+void AcrossAccountGroupTest::TearDownTestCase() {}
+void AcrossAccountGroupTest::SetUp() {}
+void AcrossAccountGroupTest::TearDown() {}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest001, TestSize.Level0)
+{
+    int32_t ret = CompareString(nullptr, nullptr);
+    EXPECT_EQ(ret, 0);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest002, TestSize.Level0)
+{
+    int32_t ret = GenerateGroupId(nullptr, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest003, TestSize.Level0)
+{
+    int32_t ret = AddCredTypeToParamsFromIdenticalGroup(DEFAULT_OS_ACCOUNT, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest004, TestSize.Level0)
+{
+    TrustedDeviceEntry devParams;
+    int32_t ret = GenerateDevParams(nullptr, nullptr, &devParams);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest005, TestSize.Level0)
+{
+    TrustedGroupEntry groupParams;
+    int32_t ret = GenerateGroupParams(nullptr, nullptr, &groupParams);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest006, TestSize.Level0)
+{
+    int32_t ret = GenerateAcrossAccountGroupId(nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest007, TestSize.Level0)
+{
+    int32_t ret = AssertIdenticalGroupExist(DEFAULT_OS_ACCOUNT, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest008, TestSize.Level0)
+{
+    int32_t ret = AssertSharedUserIdValid(nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest009, TestSize.Level0)
+{
+    int32_t ret = CheckCreateParams(DEFAULT_OS_ACCOUNT, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest010, TestSize.Level0)
+{
+    int32_t ret = GenerateAddTokenParams(nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest011, TestSize.Level0)
+{
+    TrustedDeviceEntry entry;
+    int32_t ret = GenerateDelTokenParams(&entry, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest012, TestSize.Level0)
+{
+    int32_t ret = CheckChangeParams(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest013, TestSize.Level0)
+{
+    TrustedDeviceEntry entry;
+    int32_t ret = GenerateTrustedDevParams(nullptr, nullptr, &entry);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest014, TestSize.Level0)
+{
+    int32_t ret = CheckPeerDeviceNotSelf(nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest015, TestSize.Level0)
+{
+    int32_t ret = AddDeviceAndToken(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest016, TestSize.Level0)
+{
+    int32_t ret = AddGroupAndLocalDev(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest017, TestSize.Level0)
+{
+    int32_t ret = CheckUserIdValid(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest018, TestSize.Level0)
+{
+    int32_t ret = CheckDeviceInfoValid(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest020, TestSize.Level0)
+{
+    int32_t ret = CreateGroup(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest021, TestSize.Level0)
+{
+    int32_t ret = CreateGroup(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest022, TestSize.Level0)
+{
+    int32_t ret = DeleteGroup(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(AcrossAccountGroupTest, AcrossAccountGroupTest023, TestSize.Level0)
+{
+    int32_t ret = AddMultiMembersToGroup(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+}
---
+++ test/unittest/deviceauth/unit_test/source/peer_to_peer_group_test.cpp
@@ -0,0 +1,203 @@
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cinttypes>
+#include <unistd.h>
+#include <gtest/gtest.h>
+
+#include "common_defs.h"
+#include "device_auth.h"
+#include "device_auth_defines.h"
+#include "hc_log.h"
+#include "json_utils.h"
+#include "../../../../../services/group_manager/src/group_operation/peer_to_peer_group/peer_to_peer_group.c"
+
+using namespace std;
+using namespace testing::ext;
+
+namespace {
+class PeerToPeerGroupTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+};
+
+void PeerToPeerGroupTest::SetUpTestCase() {}
+void PeerToPeerGroupTest::TearDownTestCase() {}
+void PeerToPeerGroupTest::SetUp() {}
+void PeerToPeerGroupTest::TearDown() {}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest001, TestSize.Level0)
+{
+    bool ret = IsSameNameGroupExist(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_EQ(ret, false);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest002, TestSize.Level0)
+{
+    int32_t ret = CheckGroupName(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest003, TestSize.Level0)
+{
+    int32_t ret = GenerateGroupId(nullptr, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest004, TestSize.Level0)
+{
+    int32_t ret = GeneratePeerToPeerGroupId(nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest005, TestSize.Level0)
+{
+    int32_t ret = CheckCreateParams(DEFAULT_OS_ACCOUNT, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest006, TestSize.Level0)
+{
+    TrustedGroupEntry groupParams;
+    int32_t ret = GenerateGroupParams(nullptr, nullptr, &groupParams);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest007, TestSize.Level0)
+{
+    TrustedDeviceEntry devParams;
+    int32_t ret = GenerateDevParams(nullptr, nullptr, &devParams);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest008, TestSize.Level0)
+{
+    int32_t ret = CreateGroupInner(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest009, TestSize.Level0)
+{
+    int32_t ret = GetPeerDevUserTypeFromDb(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_EQ(ret, DEVICE_TYPE_ACCESSORY);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest010, TestSize.Level0)
+{
+    int32_t ret = AddAuthIdAndUserTypeToParams(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest011, TestSize.Level0)
+{
+    DeviceAuthCallback callback;
+    int32_t ret = HandleLocalUnbind(DEFAULT_REQUEST_ID, nullptr, &callback);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest012, TestSize.Level0)
+{
+    int32_t ret = CheckPeerDeviceStatus(DEFAULT_REQUEST_ID, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest013, TestSize.Level0)
+{
+    int32_t ret = CheckInvitePeer(nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest014, TestSize.Level0)
+{
+    int32_t ret = CheckJoinPeer(nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest015, TestSize.Level0)
+{
+    int32_t ret = CheckDeletePeer(nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest016, TestSize.Level0)
+{
+    int32_t ret = CheckClientStatus(CODE_NULL, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest017, TestSize.Level0)
+{
+    CJson *ret = GenerateGroupErrorMsg(HC_SUCCESS, DEFAULT_REQUEST_ID, nullptr);
+    DeviceAuthCallback callback;
+    InformPeerProcessError(DEFAULT_REQUEST_ID, nullptr, &callback, HC_SUCCESS);
+    EXPECT_EQ(ret, nullptr);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest019, TestSize.Level0)
+{
+    DeviceAuthCallback callback;
+    int32_t ret = CreateClientSession(DEFAULT_REQUEST_ID, CODE_NULL, NO_CHANNEL, nullptr, &callback);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest020, TestSize.Level0)
+{
+    DeviceAuthCallback callback;
+    int32_t ret = CreateServerSession(DEFAULT_REQUEST_ID, CODE_NULL, nullptr, &callback);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest021, TestSize.Level0)
+{
+    int32_t ret = CreateGroup(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest022, TestSize.Level0)
+{
+    int32_t ret = CreateGroup(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest023, TestSize.Level0)
+{
+    int32_t ret = DeleteGroup(DEFAULT_OS_ACCOUNT, nullptr, nullptr);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest024, TestSize.Level0)
+{
+    DeviceAuthCallback callback;
+    int32_t ret = AddMemberToGroup(DEFAULT_OS_ACCOUNT, DEFAULT_OS_ACCOUNT, nullptr, &callback);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest025, TestSize.Level0)
+{
+    DeviceAuthCallback callback;
+    int32_t ret = DeleteMemberFromGroup(DEFAULT_OS_ACCOUNT, DEFAULT_REQUEST_ID, nullptr, &callback);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+
+HWTEST_F(PeerToPeerGroupTest, PeerToPeerGroupTest026, TestSize.Level0)
+{
+    DeviceAuthCallback callback;
+    int32_t ret = ProcessData(DEFAULT_REQUEST_ID, nullptr, &callback);
+    EXPECT_NE(ret, HC_SUCCESS);
+}
+}
