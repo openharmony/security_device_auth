63432b0ccf0c4a13920f42da3f540b3a5ddaff92
mmc_mmc <zhmao621@126.com>
2023-12-11 19:42:06 +0800

    add DT&fuzz
    Signed-off-by: mmc_mmc <zhmao621@126.com>

--- frameworks/deviceauth_lite/fuzztest/receivedata_fuzzer/receivedata_fuzzer.cpp
+++ frameworks/deviceauth_lite/fuzztest/receivedata_fuzzer/receivedata_fuzzer.cpp
@@ -74,53 +74,29 @@ static struct session_identity identity = {
     0
 };
 
-std::string MakePakeRequest()
-{
-    std::string retStr = "{\"message\":1,\"payload\":{\"version\":{\"currentVersion\":\"1.0.0\","
-        "\"minVersion\":\"1.0.0\"},\"support256mod\":\"true\","
-        "\"operationCode\":\"1\",}}";
-    return retStr;
-}
+uint8_t g_retStr1[] = "{\"message\":1,\"payload\":{\"version\":{\"currentVersion\":\"1.0.0\","
+    "\"minVersion\":\"1.0.0\"},\"support256mod\":\"true\","
+    "\"operationCode\":\"1\",}}";
 
-std::string MakePakeClientConfirm()
-{
-    std::string retStr = "{\"message\":3,\"payload\":{\"kcfData\":\"463853720FFFC312084B9FF288E17C3F3D8B9D8F2A"
-        "609D349CAA712AAD926C26\",\"challenge\":\"76539E5634EDA735A94845C3A4F356D6\","
-        "\"epk\":\"24EBF8D727B19E8A43B20D22F744113CB49B226D834B2E3C9CB5B0378732D6CF7C658BFB468682A6762"
-        "2D5FE061F4D8102E4D8912377AA785919C529F0C1289F2100E641C6DC626054FC30304DC804FD9F059F5F5D8CEAA29"
-        "A44814F10CC2A770C5BEB0BE86559E4FA85AD6E480DC2A627F5B28626E23B613EAC21101FF1C1DDA76E35A67A5A70B\"}}";
-    return retStr;
-}
+uint8_t g_retStr2[] = "{\"message\":3,\"payload\":{\"kcfData\":\"463853720FFFC312084B9FF288E17C3F3D8B9D8F2A"
+    "609D349CAA712AAD926C26\",\"challenge\":\"76539E5634EDA735A94845C3A4F356D6\","
+    "\"epk\":\"24EBF8D727B19E8A43B20D22F744113CB49B226D834B2E3C9CB5B0378732D6CF7C658BFB468682A6762"
+    "2D5FE061F4D8102E4D8912377AA785919C529F0C1289F2100E641C6DC626054FC30304DC804FD9F059F5F5D8CEAA29"
+    "A44814F10CC2A770C5BEB0BE86559E4FA85AD6E480DC2A627F5B28626E23B613EAC21101FF1C1DDA76E35A67A5A70B\"}}";
 
-std::string MakeAuthStartRequest()
-{
-    std::string retStr = "{\"authForm\":0,\"message\":17,\"payload\":{\"authData\":\"4A4EB6622524CB"
-        "BF7DC96412A82BF4CB6022F50226A201DB3B3C55\",\"challenge\":\"A1714848785F27C22B31\",\"epk\":"
-        "\"493CB95DB80320360BE5A3E3000E3E8E67371D6DCC"
-        "57D1F97937ABABC219\",\"operationCode\":\"1\",\"version\":{\"currentVersion\":"
-        "\"1.0.0\",\"minVersion\":\"1.0.0\"},\"peerAuthId\":\"6B5A16BFA24C941F4C1B094D"
-        "6F2FA8DC8A45\",\"peerUserType\":\"0\"}}";
-    return retStr;
-}
 
-std::string MakePakeAckRequest()
-{
-    std::string retStr = "{\"authForm\":0,\"message\":18,\"payload\":{\"authData\":"
-    "\"4A4EB6622524CBBF7DC96412A82BF4CB6022F50226A201DB3B3C55\"}}";
-    return retStr;
-}
+uint8_t g_retStr3[] = "{\"authForm\":0,\"message\":17,\"payload\":{\"authData\":\"4A4EB6622524CBBF7DC96412A82BF4CB6022F50226A20"
+    "1DB3B3C55\",\"challenge\":\"A1714848785F27C22B31\",\"epk\":\"493CB95DB80320360BE5A3E3000E3E8E67371D6DCC"
+    "57D1F97937ABABC219\",\"operationCode\":\"1\",\"version\":{\"currentVersion\":"
+    "\"1.0.0\",\"minVersion\":\"1.0.0\"},\"peerAuthId\":\"6B5A16BFA24C941F4C1B094D"
+    "6F2FA8DC8A45\",\"peerUserType\":\"0\"}}";
 
-std::string MakeExchangeRequest()
-{
-    std::string retStr = "{\"message\":3,\"payload\":{\"exAuthInfo\":}}";
-    return retStr;
-}
+uint8_t g_retStr4[] = "{\"authForm\":0,\"message\":18,\"payload\":{\"authData\":\"4A4EB6622524CBBF7DC96412A82BF4CB6022"
+    "F50226A201DB3B3C55\"}}";
 
-std::string MakeInformMessage()
-{
-    std::string retStr = "{\"message\":32786,\"payload\":{\"errorCode\":17}}";
-    return retStr;
-}
+uint8_t g_retStr5[] = "{\"message\":3,\"payload\":{\"exAuthInfo\":}}";
+
+uint8_t g_retStr6[] = "{\"message\":32786,\"payload\":{\"errorCode\":17}}";
 
 bool ReceiveDataFuzz(const uint8_t* data, size_t size)
 {
@@ -130,31 +106,38 @@ bool ReceiveDataFuzz(const uint8_t* data, size_t size)
     hc_handle handle = get_instance(&identity, HC_CENTRE, &callback);
     int ver = *reinterpret_cast<const int *>(data);
     int temp = ver % 6; /* 6 : total branch */
-    std::string retStr;
+    struct uint8_buff buff;
     switch (temp) {
         case PAKE_REQUEST:
-            retStr = MakePakeRequest();
+            buff.val = g_retStr1;
+            buff.size = sizeof(g_retStr1);
+            buff.length = strlen((char *)g_retStr1);
             break;
         case PAKE_CLIENT_CONFIRM:
-            retStr = MakePakeClientConfirm();
+            buff.val = g_retStr2;
+            buff.size = sizeof(g_retStr2);
+            buff.length = strlen((char *)g_retStr2);
             break;
         case AUTH_START_REQUEST:
-            retStr = MakeAuthStartRequest();
+            buff.val = g_retStr3;
+            buff.size = sizeof(g_retStr3);
+            buff.length = strlen((char *)g_retStr3);
             break;
         case PAKE_ACK_REQUEST:
-            retStr = MakePakeAckRequest();
+            buff.val = g_retStr4;
+            buff.size = sizeof(g_retStr4);
+            buff.length = strlen((char *)g_retStr4);
             break;
         case EXCHANGE_REQUEST:
-            retStr = MakeExchangeRequest();
+            buff.val = g_retStr5;
+            buff.size = sizeof(g_retStr5);
+            buff.length = strlen((char *)g_retStr5);
             break;
         default:
-            retStr = MakeInformMessage();
+            buff.val = g_retStr6;
+            buff.size = sizeof(g_retStr6);
+            buff.length = strlen((char *)g_retStr6);
             break;
-    };
-    uint8_buff buff = {
-        const_cast<unsigned char *>(reinterpret_cast<const unsigned char *>(retStr.c_str())),
-        sizeof(retStr),
-        strlen(retStr.c_str())
     };
     receive_data(handle, &buff);
     destroy(&handle);
