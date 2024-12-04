8a8b7aa3744c0141dfbffdd587f9e35bb03f079b
张跃耀 <zhangyueyao1@huawei.com>
2024-12-04 09:51:24 +0800

    Adjusting constant position.
    Signed-off-by: 张跃耀 <zhangyueyao1@huawei.com>

--- deps_adapter/key_management_adapter/impl/src/common/mbedtls_ec_adapter.c
+++ deps_adapter/key_management_adapter/impl/src/common/mbedtls_ec_adapter.c
@@ -625,6 +625,7 @@ int32_t MbedtlsBase64Decode(const char *base64Str, uint32_t strLen, uint8_t *byt
 
 bool MbedtlsIsP256PublicKeyValid(const Uint8Buff *pubKey)
 {
+    const int32_t P256_CHECK_SCALAR_VALUE = 8;
     if ((pubKey == NULL) || (pubKey->val == NULL) || (pubKey->length != P256_PUBLIC_SIZE)) {
         LOGE("Invaild P256 pubKey input.");
         return false;
@@ -653,8 +654,7 @@ bool MbedtlsIsP256PublicKeyValid(const Uint8Buff *pubKey)
     LOG_AND_GOTO_CLEANUP_IF_FAIL(ret, "Read P256 public key failed.");
     ret = mbedtls_ecp_check_pubkey(&grp, &publicKeyPoint);
     LOG_AND_GOTO_CLEANUP_IF_FAIL(ret, "Invaild point on P256.");
-    const int32_t CHECK_SCALAR_8 = 8;
-    ret = mbedtls_mpi_lset(&scalar, CHECK_SCALAR_8);
+    ret = mbedtls_mpi_lset(&scalar, P256_CHECK_SCALAR_VALUE);
     LOG_AND_GOTO_CLEANUP_IF_FAIL(ret, "Set number eight failed.");
     ret = mbedtls_ecp_mul(&grp, &returnPoint, &scalar, &publicKeyPoint, mbedtls_ctr_drbg_random, ctrDrbg);
     LOG_AND_GOTO_CLEANUP_IF_FAIL(ret, "Compute 8PK failed.");
@@ -677,6 +677,11 @@ CLEAN_UP:
 
 static int32_t SetX25519CheckScalar(mbedtls_mpi *scalar)
 {
+    const int32_t VAILD_SCALAR_VALUE_ZERO_POS0 = 0;
+    const int32_t VAILD_SCALAR_VALUE_ZERO_POS1 = 1;
+    const int32_t VAILD_SCALAR_VALUE_ZERO_POS2 = 2;
+    const int32_t VAILD_SCALAR_VALUE_ONE_POS254 = 254;
+    const int32_t CHECK_SCALAR_VALUE_ONE_POS3 = 3;
 #if defined(MBEDTLS_HAVE_INT64)
     const int32_t SCALAR_LENGTH = 4;
 #elif defined(MBEDTLS_HAVE_INT32)
@@ -684,11 +689,6 @@ static int32_t SetX25519CheckScalar(mbedtls_mpi *scalar)
 #else
     LOG_AND_GOTO_CLEANUP_IF_FAIL(HAL_ERR_NOT_SUPPORTED, "Scalar limb size unknown.");
 #endif
-    const int32_t VAILD_SCALAR_VALUE_ZERO_POS0 = 0;
-    const int32_t VAILD_SCALAR_VALUE_ZERO_POS1 = 1;
-    const int32_t VAILD_SCALAR_VALUE_ZERO_POS2 = 2;
-    const int32_t VAILD_SCALAR_VALUE_ONE_POS254 = 254;
-    const int32_t CHECK_SCALAR_VALUE_ONE_POS3 = 3;
     //2^254 + 8 * 1
     int32_t ret = mbedtls_mpi_grow(scalar, SCALAR_LENGTH);
     LOG_AND_GOTO_CLEANUP_IF_FAIL(ret, "Set scalar length failed.");
