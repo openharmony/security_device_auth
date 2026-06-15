b4c938f1da20fe948f548112da948b601c8e8485
kou-chuxiong <kouchuxiong@h-partners.com>
2026-06-15 12:06:07 +0800

    add tlv_parcel coverage
    Signed-off-by: kou-chuxiong <kouchuxiong@h-partners.com>
    Change-Id: Ice8c0ad7c64e2691a38df0c4da11c5da4b1bbd9e

--- test/unittest/common_lib_test/hc_tlv_parser/hc_tlv_parser_test.cpp
+++ test/unittest/common_lib_test/hc_tlv_parser/hc_tlv_parser_test.cpp
@@ -23,6 +23,27 @@ using namespace testing::ext;
 namespace {
 static const uint32_t TEST_BUFFER_SIZE = 32;
 
+typedef struct {
+    DECLARE_TLV_STRUCT(2)
+    TlvUint32 member1;
+    TlvUint32 member2;
+} TestTlvStruct;
+
+void InitTestTlvStruct(TestTlvStruct *tlv, unsigned short checkTag)
+{
+    unsigned int index = 0;
+    (void)memset_s(&tlv->base, sizeof(tlv->base), 0, sizeof(tlv->base));
+    tlv->base.checkTag = GetTag(checkTag, 0x1000);
+    InitTlvUint32(&tlv->member1, 0x0001);
+    tlv->offset[index++] = offsetof(TestTlvStruct, member1);
+    InitTlvUint32(&tlv->member2, 0x0002);
+    tlv->offset[index++] = offsetof(TestTlvStruct, member2);
+    tlv->offsetCount = index;
+    tlv->base.parse = ParseTlvStruct;
+    tlv->base.getlen = GetLenTlvStruct;
+    tlv->base.encode = EncodeTlvStruct;
+    tlv->base.deinit = DeinitTlvStruct;
+}
 
 class HcTlvParserTest : public testing::Test {
 };
@@ -260,5 +281,193 @@ HWTEST_F(HcTlvParserTest, DecodeTlvMessageTest002, TestSize.Level0)
     ret = DecodeTlvMessage(nullptr, nullptr, HC_FALSE);
     EXPECT_EQ(ret, HC_FALSE);
     DeleteParcel(&parcel);
+}
+
+HWTEST_F(HcTlvParserTest, DecodeTlvMessageTest003, TestSize.Level0)
+{
+    TlvUint32 tlvUint32;
+    InitTlvUint32(&tlvUint32, 0x1234);
+    tlvUint32.data = 0x5678;
+    HcParcel parcel = CreateParcel(TEST_BUFFER_SIZE * 2, TEST_BUFFER_SIZE);
+    EncodeTlvMessage((TlvBase *)&tlvUint32, &parcel);
+    uint8_t extraData[10] = {0};
+    ParcelWrite(&parcel, extraData, sizeof(extraData));
+    TlvUint32 tlvUint32Dst;
+    InitTlvUint32(&tlvUint32Dst, 0x1234);
+    HcBool ret = DecodeTlvMessage((TlvBase *)&tlvUint32Dst, &parcel, HC_FALSE);
+    EXPECT_EQ(ret, HC_FALSE);
+    DeleteParcel(&parcel);
+    tlvUint32.base.deinit((TlvBase *)&tlvUint32);
+    tlvUint32Dst.base.deinit((TlvBase *)&tlvUint32Dst);
+}
+
+HWTEST_F(HcTlvParserTest, ParseTlvBufferTest001, TestSize.Level0)
+{
+    TlvBuffer tlvBuffer;
+    InitTlvBuffer(&tlvBuffer, 0x1234);
+    HcParcel parcel = CreateParcel(TEST_BUFFER_SIZE, TEST_BUFFER_SIZE);
+    ParcelWriteUint16(&parcel, 0x1234);
+    ParcelWriteUint16(&parcel, 10);
+    uint8_t data[5] = {0};
+    ParcelWrite(&parcel, data, sizeof(data));
+    TlvBase *tlvBase = (TlvBase *)&tlvBuffer;
+    tlvBase->checkTag = 0x1234;
+    tlvBase->length = 10;
+    int32_t ret = ParseTlvBuffer(tlvBase, &parcel, HC_FALSE);
+    EXPECT_EQ(ret, TLV_FAIL);
+    DeleteParcel(&parcel);
+    tlvBuffer.base.deinit((TlvBase *)&tlvBuffer);
+}
+
+HWTEST_F(HcTlvParserTest, EncodeTlvBufferTest001, TestSize.Level0)
+{
+    TlvBuffer tlvBuffer;
+    InitTlvBuffer(&tlvBuffer, 0x1234);
+    uint8_t data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    ParcelWrite(&tlvBuffer.data, data, sizeof(data));
+    HcParcel parcel = CreateParcel(TEST_BUFFER_SIZE, TEST_BUFFER_SIZE);
+    int32_t ret = EncodeTlvBuffer((TlvBase *)&tlvBuffer, &parcel);
+    EXPECT_EQ(ret, sizeof(data));
+    DeleteParcel(&parcel);
+    tlvBuffer.base.deinit((TlvBase *)&tlvBuffer);
+}
+
+HWTEST_F(HcTlvParserTest, ParseTlvStringTest001, TestSize.Level0)
+{
+    TlvString tlvString;
+    InitTlvString(&tlvString, 0x1234);
+    HcParcel parcel = CreateParcel(TEST_BUFFER_SIZE, TEST_BUFFER_SIZE);
+    ParcelWriteUint16(&parcel, 0x1234);
+    ParcelWriteUint16(&parcel, 10);
+    uint8_t data[5] = {0};
+    ParcelWrite(&parcel, data, sizeof(data));
+    TlvBase *tlvBase = (TlvBase *)&tlvString;
+    tlvBase->checkTag = 0x1234;
+    tlvBase->length = 10;
+    int32_t ret = ParseTlvString(tlvBase, &parcel, HC_FALSE);
+    EXPECT_EQ(ret, TLV_FAIL);
+    DeleteParcel(&parcel);
+    tlvString.base.deinit((TlvBase *)&tlvString);
+}
+
+HWTEST_F(HcTlvParserTest, EncodeTlvStringTest001, TestSize.Level0)
+{
+    TlvString tlvString;
+    InitTlvString(&tlvString, 0x1234);
+    StringSetPointer(&tlvString.data, "test1234");
+    HcParcel parcel = CreateParcel(TEST_BUFFER_SIZE, TEST_BUFFER_SIZE);
+    int32_t ret = EncodeTlvString((TlvBase *)&tlvString, &parcel);
+    EXPECT_GT(ret, 0);
+    DeleteParcel(&parcel);
+    tlvString.base.deinit((TlvBase *)&tlvString);
+}
+
+HWTEST_F(HcTlvParserTest, EncodeTlvBufferWithEmptyDataTest001, TestSize.Level0)
+{
+    TlvBuffer tlvBuffer;
+    InitTlvBuffer(&tlvBuffer, 0x1234);
+    HcParcel parcel = CreateParcel(TEST_BUFFER_SIZE, TEST_BUFFER_SIZE);
+    int32_t ret = EncodeTlvBuffer((TlvBase *)&tlvBuffer, &parcel);
+    EXPECT_EQ(ret, TLV_FAIL);
+    DeleteParcel(&parcel);
+    tlvBuffer.base.deinit((TlvBase *)&tlvBuffer);
+}
+
+HWTEST_F(HcTlvParserTest, ParseTlvHeadWithTagMismatchTest001, TestSize.Level0)
+{
+    HcParcel parcel = CreateParcel(TEST_BUFFER_SIZE, TEST_BUFFER_SIZE);
+    ParcelWriteUint16(&parcel, 0x1111);
+    ParcelWriteUint16(&parcel, 4);
+    uint32_t data = 0x5678;
+    ParcelWriteUint32(&parcel, data);
+    TlvBase tlvBase = {0};
+    tlvBase.checkTag = 0x1234;
+    HcBool ret = ParseTlvHead(&tlvBase, &parcel);
+    EXPECT_EQ(ret, HC_FALSE);
+    DeleteParcel(&parcel);
+}
+
+HWTEST_F(HcTlvParserTest, ParseTlvNodeWithInsufficientDataTest001, TestSize.Level0)
+{
+    HcParcel parcel = CreateParcel(TEST_BUFFER_SIZE, TEST_BUFFER_SIZE);
+    ParcelWriteUint16(&parcel, 0x1234);
+    ParcelWriteUint16(&parcel, 100);
+    TlvBase tlvBase = {0};
+    tlvBase.checkTag = 0x1234;
+    tlvBase.parse = ParseTlvBuffer;
+    TlvBuffer tlvBuffer;
+    InitTlvBuffer(&tlvBuffer, 0x1234);
+    int32_t ret = ParseTlvNode((TlvBase *)&tlvBuffer, &parcel, HC_FALSE);
+    EXPECT_EQ(ret, TLV_FAIL);
+    DeleteParcel(&parcel);
+    tlvBuffer.base.deinit((TlvBase *)&tlvBuffer);
+}
+
+HWTEST_F(HcTlvParserTest, ParseTlvStructWithUnknownTagTest001, TestSize.Level0)
+{
+    TestTlvStruct tlvStruct;
+    InitTestTlvStruct(&tlvStruct, 0x1000);
+    
+    HcParcel parcel = CreateParcel(TEST_BUFFER_SIZE * 3, TEST_BUFFER_SIZE);
+    ParcelWriteUint16(&parcel, 0x1000);
+    ParcelWriteUint16(&parcel, 24);
+    ParcelWriteUint16(&parcel, 0x0001);
+    ParcelWriteUint16(&parcel, sizeof(uint32_t));
+    ParcelWriteUint32(&parcel, 0x11111111);
+    ParcelWriteUint16(&parcel, 0x9999);
+    ParcelWriteUint16(&parcel, sizeof(uint32_t));
+    ParcelWriteUint32(&parcel, 0x22222222);
+    ParcelWriteUint16(&parcel, 0x0002);
+    ParcelWriteUint16(&parcel, sizeof(uint32_t));
+    ParcelWriteUint32(&parcel, 0x33333333);
+    
+    int32_t ret = ParseTlvStruct((TlvBase *)&tlvStruct, &parcel, HC_FALSE);
+    EXPECT_EQ(ret, TLV_FAIL);
+    
+    DeleteParcel(&parcel);
+    tlvStruct.base.deinit((TlvBase *)&tlvStruct);
+}
+
+HWTEST_F(HcTlvParserTest, ParseTlvStructWithStrictFailTest001, TestSize.Level0)
+{
+    TestTlvStruct tlvStruct;
+    InitTestTlvStruct(&tlvStruct, 0x1000);
+    
+    HcParcel parcel = CreateParcel(TEST_BUFFER_SIZE * 2, TEST_BUFFER_SIZE);
+    ParcelWriteUint16(&parcel, 0x1000);
+    ParcelWriteUint16(&parcel, 8);
+    ParcelWriteUint16(&parcel, 0x0001);
+    ParcelWriteUint16(&parcel, sizeof(uint32_t));
+    ParcelWriteUint32(&parcel, 0x11111111);
+    
+    int32_t ret = ParseTlvStruct((TlvBase *)&tlvStruct, &parcel, HC_TRUE);
+    EXPECT_EQ(ret, TLV_FAIL);
+    
+    DeleteParcel(&parcel);
+    tlvStruct.base.deinit((TlvBase *)&tlvStruct);
+}
+
+HWTEST_F(HcTlvParserTest, ParseTlvStructSuccessTest001, TestSize.Level0)
+{
+    TestTlvStruct tlvStruct;
+    InitTestTlvStruct(&tlvStruct, 0x1000);
+    
+    HcParcel parcel = CreateParcel(TEST_BUFFER_SIZE * 2, TEST_BUFFER_SIZE);
+    ParcelWriteUint16(&parcel, 0x1000);
+    ParcelWriteUint16(&parcel, 16);
+    ParcelWriteUint16(&parcel, 0x0001);
+    ParcelWriteUint16(&parcel, sizeof(uint32_t));
+    ParcelWriteUint32(&parcel, 0x11111111);
+    ParcelWriteUint16(&parcel, 0x0002);
+    ParcelWriteUint16(&parcel, sizeof(uint32_t));
+    ParcelWriteUint32(&parcel, 0x22222222);
+    
+    int32_t ret = ParseTlvStruct((TlvBase *)&tlvStruct, &parcel, HC_TRUE);
+    EXPECT_EQ(ret, 16);
+    EXPECT_EQ(tlvStruct.member1.data, 0x11111111);
+    EXPECT_EQ(tlvStruct.member2.data, 0x22222222);
+    
+    DeleteParcel(&parcel);
+    tlvStruct.base.deinit((TlvBase *)&tlvStruct);
 }
 }
